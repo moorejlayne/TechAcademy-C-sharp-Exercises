63aa35f50a294748d94979351ca0a7de20ab4edf
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-05 21:46:28 -0600

    Remove project files, update exercise .cs files

---
+++ ExercisePage34.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+//namespace TechAcademy_Practice
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Welcome to Acme Accounting Systems");
+//            Console.WriteLine("Remember, we're \"accounting\" on you!");
+//            Console.Read();
+//        }
+//    }
+//}
---
+++ ExercisePage57.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TechAcademy_Practice2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            double hoursStudied = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
+
