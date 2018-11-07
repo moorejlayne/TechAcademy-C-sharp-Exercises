a26bc2ad69f49b72ff6c326785902f55ad679178
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-06 18:47:55 -0600

    Add files.

---
+++ ExercisePage66.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TechAcademy_Practice3
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. Multiply user input by 50
+            Console.WriteLine("Please enter a number: ");
+            double userNum1 = Convert.ToDouble(Console.ReadLine());
+            double result1 = userNum1 * 50.0;
+            Console.WriteLine("Your number multipled by 50 is " + result1.ToString());
+
+            // 2. Add 25 to user input 
+            Console.WriteLine("Please enter a number: ");
+            double userNum2 = Convert.ToDouble(Console.ReadLine());
+            double result2 = userNum2 + 25.0;
+            Console.WriteLine("Your number plus 25 is " + result2.ToString());
+
+            // 3. Divide user input by 12.5
+            Console.WriteLine("Please enter a number: ");
+            double userNum3 = Convert.ToDouble(Console.ReadLine());
+            double result3 = userNum3 / 12.5;
+            Console.WriteLine("Your number divided by 12.5 is " + result3.ToString());
+
+            // 4. Check if user input greater than 50
+            Console.WriteLine("Please enter a number: ");
+            double userNum4 = Convert.ToDouble(Console.ReadLine());
+            bool result4 = userNum4 > 50.0;
+            if(result4)
+            {
+                Console.WriteLine("Your number is greater than 50");
+            }
+            else
+            {
+                Console.WriteLine("Your number is not greater than 50");
+            }
+           
+            // 5. Remainder of user input divided by 7
+            Console.WriteLine("Please enter a number: ");
+            double userNum5 = Convert.ToDouble(Console.ReadLine());
+            double result5 = userNum5 % 7;
+            Console.WriteLine("The remainder of your number divided by 7 is " + result5.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercisePage67.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TechAcademy_Practice4
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Header
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("");
+
+            // Get Person 1 details
+            Console.WriteLine("Person 1");
+            Console.Write("Hourly rate: ");
+            double rateP1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Hours worked per week: ");
+            double hoursP1 = Convert.ToDouble(Console.ReadLine());
+
+            // Get Person 2 details
+            Console.WriteLine("Person 2");
+            Console.Write("Hourly rate: ");
+            double rateP2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Hours worked per week: ");
+            double hoursP2 = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate and compare weekly and annual salary of Person 1 and Person 2 
+            // Assumes 50 full weeks worked in a year
+            double weeklyP1 = rateP1 * hoursP1;
+            double annualP1 = weeklyP1 * 50;
+            double weeklyP2 = rateP2 * hoursP2;
+            double annualP2 = weeklyP2 * 50;
+            bool P1gP2 = annualP1 > annualP2;
+
+            // Display values to the console
+            Console.WriteLine(""); 
+            Console.WriteLine("Weekly salary of Person 1: " + weeklyP1);
+            Console.WriteLine("Annual salary of Person 1: " + annualP1);
+            Console.WriteLine("");
+            Console.WriteLine("Weekly salary of Person 2: " + weeklyP2);
+            Console.WriteLine("Annual salary of Person 2: " + annualP2);
+            Console.WriteLine("Does Person 1 make more money than Person 2? " + P1gP2);
+
+            Console.ReadLine();
+        }
+    }
+}
