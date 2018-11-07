f8c880efd8206ba781f536428da8ad7fc551a4c2
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-06 19:08:59 -0600

    Add file.

---
+++ ExercisePage75.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TechAcademy_Practice5
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Get details
+            Console.Write("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Have you ever had a DUI? Enter 'true' or 'false': ");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("How many speeding tickets do you have? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            // Determine if qualifies
+            bool qualifies = age > 15 && DUI == false && tickets <= 3;
+
+            // Print output
+            Console.WriteLine("");
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Previous DUI? " + DUI);
+            Console.WriteLine("Number of speeding tickets: " + tickets);
+            Console.WriteLine("Qualified? " + qualifies);
+
+            Console.ReadLine();
+        }
+    }
+}
