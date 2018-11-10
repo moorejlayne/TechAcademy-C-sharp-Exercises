e206676cd7032b74770b00d33d8596d1a674c923
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-10 12:04:39 -0600

    Add Exercise 115

---
+++ ExercisePage115/ManipulateNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExercisePage115
+{
+    public class ManipulateNumbers
+    {
+
+        public int AddNumbers(int inputValue1, int inputValue2 = 0)
+        {
+            return inputValue1 + inputValue2;
+        }
+    }
+}
---
+++ ExercisePage115/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExercisePage115
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // instantiate class
+            ManipulateNumbers manipulateNumbers = new ManipulateNumbers();
+
+            // get values from user
+            Console.WriteLine("Please enter an integer.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a value to add to the first number. Leave blank if you do not wish to add anything.");
+            string userInput2 = Console.ReadLine();
+
+            // check if entered second number
+            bool input2 = true;
+            try
+            {
+                Convert.ToInt32(userInput2);
+            }
+            catch
+            {
+                input2 = false;
+            }
+
+            // call method and display output
+            if (input2)
+            {
+                int returnedValue = manipulateNumbers.AddNumbers(userInput1, Convert.ToInt32(userInput2));
+                Console.WriteLine("Your first value was " + userInput1 + ". Your second value was " + userInput2 + ".");
+                Console.WriteLine("The sum of the two numbers is " + returnedValue + ".");
+            }
+            else
+            {
+                int returnedValue = manipulateNumbers.AddNumbers(userInput1);
+                Console.WriteLine("Your first value was " + userInput1 + ". Your did not enter a second number.");
+                Console.WriteLine("The final value is " + returnedValue + ".");
+            }
+
+        }
+    }
+}
+
+
