db978b1d2f4b46c1d3998538ece50274542f17dc
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-10 10:17:11 -0600

    Add ExercisePage111

---
+++ ExercisePage111/ManipulateNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ExercisePage111
+{
+    public static class ManipulateNumber
+    {
+        // method 1: add 10 to InputNumber
+        public static int Add10(int inputNumber)
+        {
+            return inputNumber + 10;
+        }
+
+        // method 2: subtract 2 from InputNumber
+        public static int Subtract2(int inputNumber)
+        {
+            return inputNumber - 2;
+        }
+
+        // method 3: multiply InputNumber by 5
+        public static int Multiply5(int inputNumber)
+        {
+            return inputNumber * 5;
+        }
+
+    }
+}
---
+++ ExercisePage111/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercisePage111
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter an integer value: ");
+            int inputValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered " + inputValue + ".");
+
+            // call method 1
+            int returnedValue1 = ManipulateNumber.Add10(inputValue);
+            Console.WriteLine(inputValue + " + 10 = " + returnedValue1);
+
+            // call method 2
+            int returnedValue2 = ManipulateNumber.Subtract2(inputValue);
+            Console.WriteLine(inputValue + " - 2 = " + returnedValue2);
+
+            // call method 3
+            int returnedValue3 = ManipulateNumber.Multiply5(inputValue);
+            Console.WriteLine(inputValue + " * 5 = " + returnedValue3);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
