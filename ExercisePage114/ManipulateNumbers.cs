4bc97abc489b95ec08d395a062c7c3c7787fef0f
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-10 11:27:31 -0600

    Add Exercise 114

---
+++ ExercisePage114/ManipulateNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercisePage114
+{
+    public class ManipulateNumbers
+    {
+        //public ManipulateNumbers()
+        //{
+        //}
+
+        public int ModifyNumber(int inputValue)
+        {
+            return inputValue + 10;
+        }
+
+        public int ModifyNumber(decimal inputValue)
+        {
+            return Convert.ToInt32(inputValue + 23.5m);
+        }
+
+        public int ModifyNumber(string inputValue)
+        {
+            return Convert.ToInt32(inputValue) + 8;
+        }
+    }
+}
---
+++ ExercisePage114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercisePage114
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 1-2
+            int number1 = 12;
+            ManipulateNumbers manipulateNumbers = new ManipulateNumbers();
+            int returnedValue = manipulateNumbers.ModifyNumber(number1);
+            Console.WriteLine("The integer is " + number1 + ". The new integer is " + returnedValue + ".");
+
+            // 3-4
+            decimal number2 = 12.0m;
+            ManipulateNumbers manipulateNumbers2 = new ManipulateNumbers();
+            int returnedValue2 = manipulateNumbers2.ModifyNumber(number2);
+            Console.WriteLine("The decimal is " + number2 + ". The new integer is " + returnedValue2 + ".");
+
+            // 5-6
+            string number3 = "12";
+            ManipulateNumbers manipulateNumbers3 = new ManipulateNumbers();
+            int returnedValue3 = manipulateNumbers3.ModifyNumber(number3);
+            Console.WriteLine("The string is \"" + number3 + "\". The new integer is " + returnedValue3 + ".");
+
+        }
+    }
+}
