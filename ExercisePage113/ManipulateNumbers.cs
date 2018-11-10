6d8824b93248fdd16068ff981fc31d30b3abdf05
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-10 11:08:01 -0600

    Add Exercise 113 files

---
+++ ExercisePage113/ManipulateNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ExercisePage113
+{
+    public class ManipulateNumbers
+    {
+        //public ManipulateNumbers()
+        //{
+        //}
+
+        public void ModifyAndDisplay(int inputValue1, int inputValue2)
+        {
+            int newValue1 = inputValue1 + 2;
+            Console.WriteLine("Your second value was " + inputValue2 + ".");
+        }
+    }
+}
---
+++ ExercisePage113/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercisePage113
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // instantiate class
+            ManipulateNumbers manipulateNumbers = new ManipulateNumbers();
+           
+            // call method
+            manipulateNumbers.ModifyAndDisplay(25, 4020);
+
+            // call method using parameter names
+            manipulateNumbers.ModifyAndDisplay(inputValue1: 30, inputValue2: 400400);
+
+
+            Console.ReadLine();
+        }
+    }
+}
