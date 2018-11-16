346d8fa6d3eef9120c365b1acabe98920bc836ce
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-15 20:35:50 -0600

    Add Exercise Page 129 Files

---
+++ ExercisePage129/Person.cs
@@ -0,0 +1,6 @@
+﻿using System; namespace ExercisePage129 {     public abstract class Person     {
+        // properties
+        public string FirstName { get; set; }         public string LastName { get; set; }
+
+        // method
+        public abstract void SayName();      } }
