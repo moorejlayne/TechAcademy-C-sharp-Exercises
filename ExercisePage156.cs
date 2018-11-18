2068a4bd03610ddfc336792baba328941b55ea79
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-18 12:50:15 -0600

    Create ExercisePage156.cs

---
+++ ExercisePage156.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace CsharpExercises
+{
+    public class ExercisePage156
+    {
+        public static void Main(string[] args)
+        {
+            // print current date and time to console
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current date and time: " + currentDateTime);
+
+            // ask user for number
+            Console.Write("Please enter a number of hours: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // add user number to datetime from when program began, and print. 
+            DateTime futureDateTime = currentDateTime.AddHours(userInput);
+            Console.WriteLine("In {0} hours it will be: " + futureDateTime, userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
