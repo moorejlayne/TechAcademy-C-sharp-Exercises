4376729b1243c3f627a4c0e127d8db424e2beba1
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-17 20:52:22 -0600

    Add Exercise Page 137

---
+++ ExercisePage137.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Number number = new Number { Amount = 1205.02m };
+
+        Console.WriteLine("Number amount is " + number.Amount.ToString("0.00") + ".");
+    }
+
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
