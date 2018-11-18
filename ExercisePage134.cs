55e9b36c4ee18f469a80046b6bd9f8e31864909d
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-17 20:15:42 -0600

    Add Exercise Page 134

---
+++ ExercisePage134.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercisePage134
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter the current day of the week: ");
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            string userDay = textInfo.ToTitleCase(Console.ReadLine());
+
+            try
+            {
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userDay);
+                Console.WriteLine("Today is " + currentDay + ".");
+            }
+            catch (System.ArgumentException) 
+            {
+                Console.WriteLine("Please enter a correct day of the week.");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        // create enum for days of the week
+        public enum DaysOfWeek
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday,
+            Saturday, Sunday
+        }
+    }
+}
