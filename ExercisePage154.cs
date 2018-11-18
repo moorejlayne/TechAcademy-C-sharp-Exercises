76a0af368a2fe1a0e46d58aaa831451cfdd9667c
Jacob L Moore <moore.jlayne@gmail.com>
2018-11-18 12:08:19 -0600

    Create ExercisePage154.cs

---
+++ ExercisePage154.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace CsharpExercises
+{
+    public class ExercisePage154
+    {
+        public static void Main(string[] args)
+        {
+            // assign path for saving / loading log file
+            string path = "/Users/JLM/Desktop/log.txt";
+
+            // ask user for number
+            Console.Write("Please enter a whole number: ");
+            string userInput = Console.ReadLine();
+
+            // log number to text file
+            File.WriteAllText(path, userInput);
+
+            // print text file back to the user
+            string fileContents = File.ReadAllText(path);
+            Console.WriteLine("The following was saved to the log file: " + fileContents);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
